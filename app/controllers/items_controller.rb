02e19b063c0aca43f23f03e14ae27f70aa1b0ebd
yudai0112 <71693999+yudai0112@users.noreply.github.com>
2020-10-25 16:44:29 +0900

    修正（if分で分岐）

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -37,9 +37,10 @@ class ItemsController < ApplicationController
     end
 
     def destroy
-      @item = Item.find(params[:id])
-      @item.destroy
-      redirect_to root_path
+        @item.destroy 
+        if @item.user.id == current_user.id
+          redirect_to root_path
+        end
     end
 
   private
