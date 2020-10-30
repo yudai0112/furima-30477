7bab9ac187bdaaca5d2aa21188accc599054e69a
yudai0112 <71693999+yudai0112@users.noreply.github.com>
2020-10-30 15:29:46 +0900

    basic認証と手数料の変更

--- app/javascript/item.js
+++ app/javascript/item.js
@@ -3,9 +3,9 @@ function price() {
   priceInput.addEventListener("input", () => {
     const inputValue = priceInput.value; 
     const addTaxDom = document.getElementById("add-tax-price");
-    addTaxDom.innerHTML = Math.floor(inputValue * 0.9)
+    addTaxDom.innerHTML = Math.floor(inputValue * 0.1)
     const profitDom = document.getElementById("profit");
-    profitDom.innerHTML = Math.floor(inputValue * 0.1)
+    profitDom.innerHTML = Math.floor(inputValue * 0.9)
   })
 }
 window.addEventListener("load", price);
